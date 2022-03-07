2db7be1613e66f491efb142460ce77385bf90c8c
aedra-v <85135938+aedra-v@users.noreply.github.com>
2022-03-07 00:03:01 -0600

    Update slackhook-cloudfxn-1.py

--- slackhook-cloudfxn-1.py
+++ slackhook-cloudfxn-1.py
@@ -1,7 +1,31 @@
 import requests
 
 
-def coffee_time(request):
+def slackhook_httpincoming(request):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    request_json = request.get_json()
+#    url = "https://hooks.slack.com/services/TEAMID/CHANNEL/WEBHOOK" ##### need to specify #
+    payload = '{"text":"It is the coffee time!"}'
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text.encode('utf8'))
+    
+def slackhook_rawimport(request):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    request_json = request.get_json()
+#    url = "https://hooks.slack.com/services/TEAMID/CHANNEL/WEBHOOK" ##### need to specify #
+    payload = '{"text":"It is the coffee time!"}'
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text.encode('utf8'))
+    
+def slackhook_dataparse(request):
     headers = {
         'Content-Type': 'application/json'
     }
